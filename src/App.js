38fb9ca22cfd317601406f635ff9f4cc340e4169
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-07 18:26:01 +0200

    transfered functionality from Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Sidebar from "./components/sidebar/Sidebar";
 import Stats from "./components/main/Stats";
 
 function App() {
+  const [API_WORLDWIDE] = useState('https://disease.sh/v3/covid-19/all?yesterday=true&strict=true');
+  const [API_COUNTRIES] = useState('https://disease.sh/v3/covid-19/countries/');
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+
+  useEffect(() => {
+   fetch(API_WORLDWIDE)
+     .then(response => response.json())
+     .then(data => setCountryInfo(data))
+  }, [API_WORLDWIDE]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const response = await fetch(API_COUNTRIES);
+      const data = await response.json();
+      const countries = data.map(country => ({
+        name: country.country,
+        value: country.countryInfo.iso2,
+      }))
+      setCountries(countries);
+    }
+    getCountries();
+  }, [API_COUNTRIES]);
+
+  const onCountryChange = async event => {
+    const countryCode = event.target.value;
+    const url =
+      countryCode === 'worldwide' ?
+      API_WORLDWIDE :
+      API_COUNTRIES
+        .concat(countryCode)
+        .concat('?yesterday=true&strict=true');
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
+  };
+
   return (
-    <div>
-      <Header />
-      <Stats />
+    <div className='app'>
+      <div className='app__left'>
+        <Header country={country} countries={countries} onCountryChange={onCountryChange} />
+        <Stats countryInfo={countryInfo} />
+      </div>
+
+      <div className='app__right'>
+        <Sidebar />
+      </div>
     </div>
   );
 }
