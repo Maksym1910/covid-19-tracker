b9d90e2f8ccec34aa196e97422abc8d518c0c9ff
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-07 18:29:29 +0200

    updated Header component & index.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,44 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Header.css';
 import {MenuItem, FormControl, Select} from '@material-ui/core';
 
-function Header() {
-  const [API] = useState('https://disease.sh/v3/covid-19/countries');
-  const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide');
-
-  useEffect(() => {
-    const getCountries = async () => {
-      const response = await fetch(API);
-      const data = await response.json();
-      const countries = data.map(country => ({
-        name: country.country,
-        value: country.countryInfo.iso2,
-      }))
-      setCountries(countries);
-    }
-    getCountries();
-  }, [API]);
-
-  const onCountryChange = event => {
-    const countryCode = event.target.value;
-    setCountry(countryCode)
-  }
-
+const Header = props => {
   return (
-    <header className='header'>
-      <h1 className='header__title'>COVID-19 TRACKER</h1>
-      <FormControl className='header__dropdown'>
-        <Select variant='outlined' onChange={onCountryChange} value={country}>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          {
-            countries.map(country => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))
-          }
-        </Select>
-      </FormControl>
-    </header>
+      <header className='header'>
+        <h1 className='header__title'>COVID-19 TRACKER</h1>
+        <FormControl className='header__dropdown'>
+          <Select variant='outlined' onChange={props.onCountryChange} value={props.country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {
+              props.countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </header>
   )
 }
 
