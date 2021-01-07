943fffd89f01ee13fe73cf10993f9d640e287c69
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-07 18:31:13 +0200

    added sidebar component

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card, CardContent} from "@material-ui/core";
+
+const Sidebar = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h3>Live Cases by Country</h3>
+        <h4>Worldwide new cases</h4>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Sidebar;
