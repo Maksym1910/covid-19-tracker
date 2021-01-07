40697045ad5afe3ae63a15d9da9ec4c968ddce63
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-07 18:32:15 +0200

    updated Stats & InfoBox components

--- src/components/main/Stats.js
+++ src/components/main/Stats.js
@@ -3,13 +3,14 @@ import './Stats.css'
 import InfoBox from "./infobox/InfoBox";
 import Map from "./map/Map";
 
-const Stats = () => {
+const Stats = props => {
+
   return (
     <div>
       <div className='stats'>
-        <InfoBox title='Coronavirus cases' cases={2000} total={3000}/>
-        <InfoBox title='Recovered' cases={2000} total={3000}/>
-        <InfoBox title='Deaths' cases={2000} total={3000}/>
+        <InfoBox title='Coronavirus cases' cases={props.countryInfo.todayCases} total={props.countryInfo.cases}/>
+        <InfoBox title='Recovered' cases={props.countryInfo.todayRecovered} total={props.countryInfo.recovered}/>
+        <InfoBox title='Deaths' cases={props.countryInfo.todayDeaths} total={props.countryInfo.deaths}/>
       </div>
       <Map />
     </div>
