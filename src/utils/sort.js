6b220103a89d19dc0a29ec2484c709e9ed4fc981
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-08 14:26:45 +0200

    refactored sort function

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,8 +1,5 @@
 export const sortData = (data) => {
   const sortedData = [...data];
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) return -1;
-    else return 1;
-  });
+  sortedData.sort((a, b) => b.cases - a.cases);
   return sortedData;
 };
