d17da3db10d134bd82105db6e4e9e81be6f14aa6
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-09 22:27:57 +0200

    stylized circles on map

--- src/utils/showDataOnMap.js
+++ src/utils/showDataOnMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import numeral from "numeral";
 import { Circle, Popup } from "react-leaflet";
 
 const casesTypeColors = {
@@ -29,9 +30,22 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <h1>Hello</h1>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
       </Popup>
     </Circle>
   ));
-
-
