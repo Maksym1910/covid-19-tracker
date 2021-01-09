52645d062bfc507f4473f214c79dfefcd0a2fcc8
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-09 16:58:43 +0200

    refactored Stats component

--- src/components/main/Stats.js
+++ src/components/main/Stats.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import './Stats.css';
+import React from "react";
+import "./Stats.css";
 import InfoBox from "./infobox/InfoBox";
 import Map from "./map/Map";
 
 const Stats = ({ countryInfo }) => {
-
   return (
     <div>
-      <div className='stats'>
-          <InfoBox title='Coronavirus cases' cases={countryInfo.todayCases} total={countryInfo.cases} />
-          <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
-          <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+      <div className="stats">
+        <InfoBox
+          title="Coronavirus cases"
+          cases={countryInfo.todayCases}
+          total={countryInfo.cases}
+        />
+        <InfoBox
+          title="Recovered"
+          cases={countryInfo.todayRecovered}
+          total={countryInfo.recovered}
+        />
+        <InfoBox
+          title="Deaths"
+          cases={countryInfo.todayDeaths}
+          total={countryInfo.deaths}
+        />
       </div>
       <Map />
     </div>
-
   );
 };
 
