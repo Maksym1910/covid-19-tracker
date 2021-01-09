f04f0005cadfc6754c0b4264f36d7f31857a83e5
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-09 14:37:30 +0200

    updated dependecies & stylized global & LineGraph components

--- src/components/sidebar/LineGraph.js
+++ src/components/sidebar/LineGraph.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { options } from "../../utils/options";
 import { buildChartData } from "../../utils/buildChartData";
+import './LineGraph.css';
 
 const LineGraph = ({ casesType = "cases" }) => {
   const [API] = useState(
@@ -22,7 +23,7 @@ const LineGraph = ({ casesType = "cases" }) => {
   }, [casesType]);
 
   return (
-    <div>
+    <div className='chart'>
       {data?.length > 0 && (
         <Line
           data={{
