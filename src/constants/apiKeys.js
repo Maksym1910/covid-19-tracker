8dfe2aad8f8989f8caf07c0fb85176a6789e9d32
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2021-01-13 07:27:09 +0200

    created constant folder

---
+++ src/constants/apiKeys.js
@@ -0,0 +1,3 @@
+export const API_WORLDWIDE = 'https://disease.sh/v3/covid-19/all?yesterday=true&strict=true';
+export const API_COUNTRIES = 'https://disease.sh/v3/covid-19/countries/';
+export const API_LAST_120_DAYS = 'https://disease.sh/v3/covid-19/historical/all?lastdays=120';
